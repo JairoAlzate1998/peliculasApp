016fd88c0577f01b58ab22553918ded3f8ae9bcc
Jairo Alzate <jairo.alzate@siesa.com>
2021-10-07 11:19:49 -0500

    Add favorites

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,62 +1,81 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { RespuestaMDB, PeliculaDetalle, RespuestaCredits } from '../interfaces/interfaces';
-
+import { Genre } from '../interfaces/interfaces';
+import {
+  RespuestaMDB,
+  PeliculaDetalle,
+  RespuestaCredits,
+} from '../interfaces/interfaces';
 
 const URL = environment.url;
 const apiKey = environment.apiKey;
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MoviesService {
+  private popularesPage: number = 0;
+  generos: Genre[] = [];
 
-  private popularesPage: number = 0; 
+  constructor(private _httpClientModule: HttpClient) {}
 
-  constructor( private _httpClientModule: HttpClient ) { }
-
-  private ejecutarQuery<T>(query: string){
+  private ejecutarQuery<T>(query: string) {
     query = URL + query;
-    query += `&api_key=${ apiKey }&language=es&include_image_language=es`;
-    
-    return this._httpClientModule.get<T>( query );
+    query += `&api_key=${apiKey}&language=es&include_image_language=es`;
+
+    return this._httpClientModule.get<T>(query);
   }
 
-  getFeature(){
-
+  getFeature() {
     const hoy = new Date();
-    const ultimoDay  = new Date( hoy.getFullYear(), hoy.getMonth() + 1, 0).getDate();
+    const ultimoDay = new Date(
+      hoy.getFullYear(),
+      hoy.getMonth() + 1,
+      0
+    ).getDate();
 
     let mes = hoy.getMonth() + 1;
     let mesString;
 
-    if( mes < 10 ){
+    if (mes < 10) {
       mesString = '0' + mes;
-    }else {
+    } else {
       mesString = mes;
     }
 
-    const inicio = `${ hoy.getFullYear() }-${ mesString }-01`;
-    const fin = `${ hoy.getFullYear() }-${ mesString }-${ ultimoDay }`;
+    const inicio = `${hoy.getFullYear()}-${mesString}-01`;
+    const fin = `${hoy.getFullYear()}-${mesString}-${ultimoDay}`;
 
-    return this.ejecutarQuery<RespuestaMDB>(`/discover/movie?primary_release_date.gte=${ inicio }&primary_release_date.lte=${ fin }`);
+    return this.ejecutarQuery<RespuestaMDB>(
+      `/discover/movie?primary_release_date.gte=${inicio}&primary_release_date.lte=${fin}`
+    );
   }
 
-  getPopular(){
+  getPopular() {
     this.popularesPage++;
     const query = `/discover/movie?sort_by=popularity.desc&page=${this.popularesPage}`;
     return this.ejecutarQuery<RespuestaMDB>(query);
   }
 
-  getPeliculaDetalle(id: string){
-    return this.ejecutarQuery<PeliculaDetalle>(`/movie/${ id }?a=1`);
+  getPeliculaDetalle(id: string) {
+    return this.ejecutarQuery<PeliculaDetalle>(`/movie/${id}?a=1`);
   }
 
-  getActoresPelicula(id: string){
-    return this.ejecutarQuery<RespuestaCredits>(`/movie/${ id }/credits?a=1`);
+  getActoresPelicula(id: string) {
+    return this.ejecutarQuery<RespuestaCredits>(`/movie/${id}/credits?a=1`);
   }
 
-  buscarPeliculas( texto: string ){
-    return this.ejecutarQuery(`/search/movie?query=${ texto }`);
+  buscarPeliculas(texto: string) {
+    return this.ejecutarQuery(`/search/movie?query=${texto}`);
+  }
+
+  cargarGeneros(): Promise<Genre[]> {
+    return new Promise((resolve) => {
+      this.ejecutarQuery(`/genre/movie/list?a=1`).subscribe((res) => {
+        this.generos = res['genres'];
+        console.log(this.generos);
+        resolve(this.generos);
+      });
+    });
   }
 }
